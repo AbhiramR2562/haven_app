fc0428aea55f8860e85df9f837cb35227a415d5d
AbhiramR2562 <abhiram2562@gmail.com>
2025-01-05 11:47:46 +0530

    updated product tile

--- lib/widget/product_tile_widget.dart
+++ lib/widget/product_tile_widget.dart
@@ -20,96 +20,103 @@ class ProductTile extends StatelessWidget {
         color: AppTheme.primaryColor,
         borderRadius: BorderRadius.circular(18),
       ),
-      child: Column(
+      child: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 0, top: 8, bottom: 8),
-            child: ClipRRect(
-              // borderRadius: const BorderRadius.vertical(top: Radius.circular(8)),
-              child: Image.asset(
-                product.imageUrl,
-                width: 115,
-                height: 116,
-                fit: BoxFit.cover,
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 0, top: 8, bottom: 8),
+                child: ClipRRect(
+                  // borderRadius: const BorderRadius.vertical(top: Radius.circular(8)),
+                  child: Image.asset(
+                    product.imageUrl,
+                    width: 115,
+                    height: 116,
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 8),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    // mainAxisAlignment: MainAxisAlignment.start,
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        product.name,
-                        style: const TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        product.category,
-                        style: TextStyle(
-                          color: Colors.grey[900],
-                          fontSize: 12,
-                          fontWeight: FontWeight.w300,
-                        ),
-                      ),
-
-                      // Price
-                      Row(
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        // mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          const Text(
-                            "\$",
-                            style: TextStyle(
-                              color: AppTheme.secondaryColor,
+                          Text(
+                            product.name,
+                            style: const TextStyle(
                               fontSize: 12,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
                           Text(
-                            "${product.price}",
+                            product.category,
                             style: TextStyle(
                               color: Colors.grey[900],
                               fontSize: 12,
-                              fontWeight: FontWeight.bold,
+                              fontWeight: FontWeight.w300,
                             ),
                           ),
+
+                          // Price
+                          Row(
+                            children: [
+                              const Text(
+                                "\$",
+                                style: TextStyle(
+                                  color: AppTheme.secondaryColor,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                "${product.price}",
+                                style: TextStyle(
+                                  color: Colors.grey[900],
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 19.78,
+                          )
                         ],
                       ),
-                      const SizedBox(
-                        height: 19.78,
-                      )
                     ],
                   ),
-                  Container(
-                    width: 36,
-                    height: 36,
-                    decoration: const BoxDecoration(
-                      color: AppTheme.buttonColor,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(12),
-                          bottomRight: Radius.circular(12)),
-                    ),
-                    child: Center(
-                      child: IconButton(
-                          onPressed: onTap,
-                          icon: const Icon(
-                            Icons.add,
-                            color: Colors.white,
-                          )),
-                    ),
-                  )
-                ],
+                ),
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Container(
+              width: 36,
+              height: 36,
+              decoration: const BoxDecoration(
+                color: AppTheme.buttonColor,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(12),
+                    bottomRight: Radius.circular(12)),
+              ),
+              child: Center(
+                child: IconButton(
+                    onPressed: onTap,
+                    icon: const Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    )),
               ),
             ),
-          ),
+          )
         ],
       ),
     );
